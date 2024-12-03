e3d9c02f970cfbf413abe6be6fb64ae0aa8f5d40
Matt-Boeren <matt.boeren@hotmail.com>
2024-12-03 22:03:15 +0100

    main function in folders fix

--- day2/day2.go
+++ day2/day2.go
@@ -1,16 +1,16 @@
 package main
 
-import(
+import (
 	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
-func main (){
-	file, err :=  os.Open("day2Input.txt")
+func main() {
+	file, err := os.Open("day2Input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,24 +22,24 @@ func main (){
 		lineInput := scanner.Text()
 		splitInput := strings.Split(lineInput, " ")
 		var report []int
-		for i := 0; i < len(splitInput); i++{
-			
+		for i := 0; i < len(splitInput); i++ {
+
 			num, err := strconv.Atoi(splitInput[i])
-			if err != nil{
+			if err != nil {
 				fmt.Println("can't convert this to an int!")
-			}else{
+			} else {
 				report = append(report, num)
 			}
 		}
 		result := checkReport(report)
-		if result == true{
+		if result == true {
 			amountOfSafe++
-		}else{
-			for i := 0; i < len(report); i++{
+		} else {
+			for i := 0; i < len(report); i++ {
 				newReport := append([]int(nil), report...)
 				newReport = append(newReport[:i], newReport[i+1:]...)
 				newResult := checkReport(newReport)
-				if newResult == true{
+				if newResult == true {
 					amountOfSafe++
 					break
 				}
@@ -47,34 +47,34 @@ func main (){
 		}
 	}
 	fmt.Println(amountOfSafe)
-	if err := scanner.Err(); err != nil{
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
-func checkReport(l []int) bool{
+func checkReport(l []int) bool {
 	increase := false
 	list := l
 	safe := true
-	for i := 0; i < len(list) - 1; i++{
-		if i == 0 && list[i] < list[i+1]{
+	for i := 0; i < len(list)-1; i++ {
+		if i == 0 && list[i] < list[i+1] {
 			increase = true
 		}
-		if increase == true{
-			if list[i] >= list[i+1]{
+		if increase == true {
+			if list[i] >= list[i+1] {
 				safe = false
 				break
-			}else{
-				if list[i+1] - list[i] > 3{
+			} else {
+				if list[i+1]-list[i] > 3 {
 					safe = false
 					break
 				}
 			}
-		}else{
-			if list[i] <= list[i+1]{
+		} else {
+			if list[i] <= list[i+1] {
 				safe = false
 				break
-			}else{
-				if list[i] - list[i+1] > 3{
+			} else {
+				if list[i]-list[i+1] > 3 {
 					safe = false
 					break
 				}
@@ -83,4 +83,3 @@ func checkReport(l []int) bool{
 	}
 	return safe
 }
-
