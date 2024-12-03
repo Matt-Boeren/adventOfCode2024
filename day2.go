9e9f1345df485ddad45b01d45be4bc80729361f4
Matt-Boeren <matt.boeren@hotmail.com>
2024-12-03 17:43:14 +0100

    day2 part2

--- day2.go
+++ day2.go
@@ -20,48 +20,30 @@ func main (){
 	amountOfSafe := 0
 	for scanner.Scan() {
 		lineInput := scanner.Text()
-
-		result := strings.Split(lineInput, " ")
-		var list []int
-		for i := 0; i < len(result); i++{
+		splitInput := strings.Split(lineInput, " ")
+		var report []int
+		for i := 0; i < len(splitInput); i++{
 			
-			num, err := strconv.Atoi(result[i])
+			num, err := strconv.Atoi(splitInput[i])
 			if err != nil{
 				fmt.Println("can't convert this to an int!")
 			}else{
-				list = append(list, num)
+				report = append(report, num)
 			}
 		}
-		increase := false
-		safe := true
-		for i := 0; i < len(list) - 1; i++{
-			if i == 0 && list[i] < list[i+1]{
-				increase = true
-			}
-			if increase == true{
-				if list[i] >= list[i+1]{
-					safe = false
-					break
-				}else{
-					if list[i+1] - list[i] > 3{
-						safe = false
-						break
-					}
-				}
-			}else{
-				if list[i] <= list[i+1]{
-					safe = false
-					break
-				}else{
-					if list[i] - list[i+1] > 3{
-						safe = false
-						break
-					}
-				}
-			}
-		}
-		if safe == true{
+		result := checkReport(report)
+		if result == true{
 			amountOfSafe++
+		}else{
+			for i := 0; i < len(report); i++{
+				newReport := append([]int(nil), report...)
+				newReport = append(newReport[:i], newReport[i+1:]...)
+				newResult := checkReport(newReport)
+				if newResult == true{
+					amountOfSafe++
+					break
+				}
+			}
 		}
 	}
 	fmt.Println(amountOfSafe)
@@ -69,3 +51,36 @@ func main (){
 		log.Fatal(err)
 	}
 }
+func checkReport(l []int) bool{
+	increase := false
+	list := l
+	safe := true
+	for i := 0; i < len(list) - 1; i++{
+		if i == 0 && list[i] < list[i+1]{
+			increase = true
+		}
+		if increase == true{
+			if list[i] >= list[i+1]{
+				safe = false
+				break
+			}else{
+				if list[i+1] - list[i] > 3{
+					safe = false
+					break
+				}
+			}
+		}else{
+			if list[i] <= list[i+1]{
+				safe = false
+				break
+			}else{
+				if list[i] - list[i+1] > 3{
+					safe = false
+					break
+				}
+			}
+		}
+	}
+	return safe
+}
+
